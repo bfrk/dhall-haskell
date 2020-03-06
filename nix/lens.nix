db1387b07c8358d5386407e57218b3ec8caaf52e
Simon Jakobi <simon.jakobi@gmail.com>
2020-03-06 18:24:46 +0100

    Update lens.nix
    ... for compatibility with th-abstraction

--- nix/lens.nix
+++ nix/lens.nix
@@ -1,5 +1,5 @@
-{ mkDerivation, array, base, base-orphans, bifunctors, bytestring
-, Cabal, cabal-doctest, call-stack, comonad, containers
+{ mkDerivation, array, base, base-compat, base-orphans, bifunctors
+, bytestring, Cabal, cabal-doctest, call-stack, comonad, containers
 , contravariant, criterion, deepseq, directory, distributive
 , doctest, exceptions, filepath, free, generic-deriving, ghc-prim
 , hashable, HUnit, kan-extensions, mtl, nats, parallel, profunctors
@@ -7,20 +7,20 @@
 , stdenv, tagged, template-haskell, test-framework
 , test-framework-hunit, test-framework-quickcheck2
 , test-framework-th, text, th-abstraction, transformers
-, transformers-compat, unordered-containers, vector, void
+, transformers-compat, unordered-containers, vector
 }:
 mkDerivation {
   pname = "lens";
-  version = "4.17";
-  sha256 = "473664de541023bef44aa29105abbb1e35542e9254cdc846963183e0dd3f08cc";
+  version = "4.19.1";
+  sha256 = "03cf1b09825e9d92c2dfec40cbddd651313c3994855b19c130e40c4a8fe56c61";
   setupHaskellDepends = [ base Cabal cabal-doctest filepath ];
   libraryHaskellDepends = [
     array base base-orphans bifunctors bytestring call-stack comonad
     containers contravariant distributive exceptions filepath free
     ghc-prim hashable kan-extensions mtl parallel profunctors
-    reflection semigroupoids semigroups tagged template-haskell text
+    reflection semigroupoids tagged template-haskell text
     th-abstraction transformers transformers-compat
-    unordered-containers vector void
+    unordered-containers vector
   ];
   testHaskellDepends = [
     base bytestring containers deepseq directory doctest filepath
@@ -30,7 +30,7 @@ mkDerivation {
     unordered-containers vector
   ];
   benchmarkHaskellDepends = [
-    base bytestring comonad containers criterion deepseq
+    base base-compat bytestring comonad containers criterion deepseq
     generic-deriving transformers unordered-containers vector
   ];
   homepage = "http://github.com/ekmett/lens/";
